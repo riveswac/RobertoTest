3fc2019b35d2802af3c0f0eaca6b896097d8cbe9
Rob <riveswac@gmail.com>
2016-07-28 08:02:03 -0400

    created event handlers for button controls

--- Phoneword/MainActivity.cs
+++ Phoneword/MainActivity.cs
@@ -20,7 +20,44 @@ namespace Phoneword
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNUmberText);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button callButton = FindViewById<Button>(Resource.Id.CallButton);
 
+            //disable the call button
+            callButton.Enabled = false;
+
+            string translatedNumber = string.Empty;
+
+            translateButton.Click += (object sender, EventArgs e) =>
+            {
+                translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (String.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = "Call " + translatedNumber;
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (object sender, EventArgs e) =>
+            {
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage("Call " + translatedNumber + "?");
+                callDialog.SetNeutralButton("Call", delegate
+                {
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+                    StartActivity(callIntent);
+                });
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                callDialog.Show();
+            };
         }
     }
 }
