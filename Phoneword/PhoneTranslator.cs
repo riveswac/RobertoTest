3ee856e06896516f6498fd0659e98899cf1d4f81
Rob <riveswac@gmail.com>
2016-07-28 07:53:00 -0400

    implemented translator

--- Phoneword/PhoneTranslator.cs
+++ Phoneword/PhoneTranslator.cs
@@ -1,1 +1,84 @@
-﻿
+﻿using System;
+using System.Text;
+
+namespace Core
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+            {
+                return "";
+            }
+            else
+            {
+                raw = raw.ToUpperInvariant();
+            }
+
+            var newNumber = new StringBuilder();
+            foreach(var c in raw)
+            {
+                if (" -01234567890".Contains(c))
+                {
+                    newNumber.Append(c);
+                }
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                    {
+                        newNumber.Append(result);
+                    }
+                }
+            }
+
+            return newNumber.ToString();
+        }
+
+        static bool Contains (this string keystring, char c)
+        {
+            return keystring.IndexOf(c) >= 0;
+        }
+
+        static int? TranslateToNumber(char c)
+        {
+            if ("ABC".Contains(c))
+            {
+                return 2;
+            }
+            else if ("DEF".Contains(c))
+            {
+                return 3;
+            }
+            else if ("GHI".Contains(c))
+            {
+                return 4;
+            }
+            else if ("JKL".Contains(c))
+            {
+                return 5;
+            }
+            else if ("MNO".Contains(c))
+            {
+                return 6;
+            }
+            else if ("PQRS".Contains(c))
+            {
+                return 7;
+            }
+            else if ("TUV".Contains(c))
+            {
+                return 8;
+            }
+            else if ("WXYZ".Contains(c))
+            {
+                return 9;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
